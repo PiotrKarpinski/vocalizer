0b048ab74bd4ec0137dcdefabf38da75cc2b163b
Piotr <pgmkarpinski@gmail.com>
2020-02-23 17:11:43 +0100

    add full letters library and filter and sorting func

--- src/Sound.js
+++ src/Sound.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './Sound.css';
 import {letters} from './data/letters.json';
 
+
+
+
+
 class Sound extends React.Component {
 	state = {
 		play: false,
 	}
 
-	
-	
-
-	
+		
 
 
 
@@ -23,24 +24,56 @@ class Sound extends React.Component {
 
   }
 
-  playSentence = () => {
+  handleClick = () => {
+
+	if (this.props.text !== undefined) {
+	const textArr = Array.from(this.props.text)
+	console.log(textArr)
+
+	console.log(letters)
+
+	let filteredArray=[]
+
+
+
+	for(let lett of textArr) {
+		console.log(lett)
+		for(let obj of letters) {
+				if (lett === obj.letter) {
+					console.log(obj.letter)
+				filteredArray.push(obj.url)
+			}		
+		}
+
+	}
+	
+
+	filteredArray.sort(function(a, b){  
+  		return textArr.indexOf(a) - textArr.indexOf(b);
+	});
+
+	
+	console.log(filteredArray)
+	this.playSentence(filteredArray)
+  }
+}  
+
+  playSentence = (filteredArray) => {
 
 	var audio = new Audio(),
 		    i = 0;
-		var playlist = new Array(letters);
-  	console.log(letters)
+		var playlist = new Array(filteredArray);
   		
 		audio.addEventListener('ended', function () {
 		    i++
-		    console.log(letters[i])
-		    if (i<letters.length) {		    
-		    audio.src = letters[i].url;
+		    if (i<filteredArray.length) {		    
+		    audio.src = filteredArray[i];
 		    audio.play();}
 
 		}, true)
 		audio.volume = 0.3;
 		audio.loop = false;
-		audio.src = letters[0].url;
+		audio.src = filteredArray[0];
 		audio.play();
 		
 
@@ -52,11 +85,9 @@ class Sound extends React.Component {
 	render() {
 
 
-
-
 	  return (
 	    <div className='sound'>
-	    	<button type='button' onClick={this.playSentence} className='play'><i className='fa fa-volume-up'/></button>
+	    	<button type='button' onClick={this.handleClick} className='play'><i className='fa fa-volume-up'/></button>
 	    </div>
 	  );
 	}
--- src/Text.js
+++ src/Text.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import './Text.css';
+import Sound from './Sound.js';
 
 
-function Text() {
-  return (
-    <div className='text'>
-    	<input placeholder="Type some sentence here"/>
-    </div>
-  );
+
+class Text extends React.Component {
+
+
+
+
+    constructor() {
+        super();
+        this.state = {
+          placeholder:'Type some sentence here',
+        };
+
+
+    this.handleChange = (e) => {
+    	this.setState({placeholder: e.target.value})
+    }
+    }
+
+
+    render() {    
+
+
+
+
+        return (
+            <div className='box'>
+            	<div className='text'>
+                <input onChange={this.handleChange} placeholder='Type some sentence here' />
+                </div>
+            	<Sound text={this.state.placeholder}/>     
+            </div>
+        );
+    }
 }
+
 
 export default Text;
